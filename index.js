feef268075354a86aee7b7c6aa4253713b7a5221
Clay Smith <casmith@users.noreply.github.com>
2021-01-31 23:38:41 -0800

    Upgrade calver

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
 'use strict';
 
-const Plugin = require('release-it/lib/plugin/Plugin'),
-    Calver = require('calver'),
-    DEFAULT_FORMAT = 'YY.MM.MICRO';
-
-class CalverPlugin extends Plugin {
-
-    getFormat() {
-        return this.getContext().format || DEFAULT_FORMAT;
-    }
-
-    getIncrementedVersion({latestVersion}) {
-        let calver = new Calver(this.getFormat(), latestVersion).inc();
-        if (calver.get() === latestVersion) {
-            calver = calver.inc('micro');
-        }
-        return calver.get();
-    }
-
-    getIncrementedVersionCI() {
-        return this.getIncrementedVersion(...arguments);
-    }
-
-    getIncrement() {
-        return this.getIncrementedVersion(...arguments);
-    }
-}
-
-module.exports = CalverPlugin;
+return require('./calver-plugin');
