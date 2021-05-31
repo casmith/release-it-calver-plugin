8a25e8951cb5e82a8c5421c1da5c00db34d289cf
Clay Smith <casmith@users.noreply.github.com>
2021-05-31 15:39:04 -0700

    Add test for bumping calendar

--- calver-plugin-tests.js
+++ calver-plugin-tests.js
@@ -1,13 +1,25 @@
 const expect    = require("chai").expect;
 const CalverPlugin = require("./calver-plugin");
 
-describe('plugin', function () {
-  	it('should bump micro when incrementing', function () {
-		const now = new Date();
-        const year = now.getFullYear().toString().substr(2);
-        const month = new Date().getMonth() + 1;
+const formatMonth = (date) => "" + (date.getMonth() + 1);
+const formatYear = (date) => "" + (date.getFullYear() - 2000);
 
-        const incrementedVersion = new CalverPlugin().getIncrementedVersion({latestVersion: `${year}.${month}.0`});
-    	expect(incrementedVersion).to.equal(`${year}.${month}.1`);
-  	});
+describe('plugin', function () {
+  it('should bump calendar when incrementing in a new month', function () {
+
+    const before = new Date();
+    before.setMonth(before.getMonth() - 2);
+    console.log(before)
+    const latestVersion = `${formatYear(before)}.${formatMonth(before)}.0`;
+    console.log(latestVersion)
+    const incrementedVersion = new CalverPlugin().getIncrementedVersion({latestVersion});
+
+    const now = new Date();
+    expect(incrementedVersion).to.equal(`${formatYear(now)}.${formatMonth(now)}.0`);
+  });
+  it('should bump micro when incrementing twice in the same month', function () {
+    const now = new Date();
+    const incrementedVersion = new CalverPlugin().getIncrementedVersion({latestVersion: `${formatYear(now)}.${formatMonth(now)}.0`});
+    expect(incrementedVersion).to.equal(`${formatYear(now)}.${formatMonth(now)}.1`);
+  });    
 });
