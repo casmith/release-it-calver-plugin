2918876c2e06220bd87ab5703da8c872b495e290
Clay Smith <casmith@users.noreply.github.com>
2021-12-21 08:03:21 -0600

    Support increment option

--- calver-plugin-tests.js
+++ calver-plugin-tests.js
@@ -2,16 +2,19 @@ const expect    = require("chai").expect;
 const CalverPlugin = require("./calver-plugin");
 
 const formatMonth = (date) => "" + (date.getMonth() + 1);
-const formatYear = (date) => "" + (date.getFullYear() - 2000);
+const formatYear = (date, fullYear = false) => "" + (date.getFullYear() - (fullYear ? 0 : 2000));
+
+function versionFromDate(date, micro = 0, fullYear = false) {
+  return `${formatYear(date, fullYear)}.${formatMonth(date)}.${micro}`;
+}
+
 
 describe('plugin', function () {
   it('should bump calendar when incrementing in a new month', function () {
 
     const before = new Date();
     before.setMonth(before.getMonth() - 2);
-    console.log(before)
-    const latestVersion = `${formatYear(before)}.${formatMonth(before)}.0`;
-    console.log(latestVersion)
+    const latestVersion = versionFromDate(before);
     const incrementedVersion = new CalverPlugin().getIncrementedVersion({latestVersion});
 
     const now = new Date();
@@ -19,7 +22,25 @@ describe('plugin', function () {
   });
   it('should bump micro when incrementing twice in the same month', function () {
     const now = new Date();
-    const incrementedVersion = new CalverPlugin().getIncrementedVersion({latestVersion: `${formatYear(now)}.${formatMonth(now)}.0`});
-    expect(incrementedVersion).to.equal(`${formatYear(now)}.${formatMonth(now)}.1`);
+    const incrementedVersion = new CalverPlugin().getIncrementedVersion({latestVersion: versionFromDate(now)});
+    expect(incrementedVersion).to.equal(versionFromDate(now, 1));
+  });
+
+  it('should acccept an increment option', function () {
+    const before = new Date();
+    before.setMonth(before.getMonth() - 2);
+    const latestVersion = versionFromDate(before);
+    const plugin = new CalverPlugin();
+    plugin.setContext({increment: 'micro'});
+    const incrementedVersion = plugin.getIncrementedVersion({latestVersion});
+    expect(incrementedVersion).to.equal(versionFromDate(before, 1));
+  });
+  it('should accept a format option', function () {
+    const now = new Date();
+    const latestVersion = versionFromDate(now, 0, true);
+    const plugin = new CalverPlugin();
+    plugin.setContext({format: "YYYY.MM.MICRO"});
+    const incrementedVersion = plugin.getIncrementedVersion({latestVersion});
+    expect(incrementedVersion).to.equal(versionFromDate(now, 1, true));
   });    
 });
--- calver-plugin.js
+++ calver-plugin.js
@@ -2,7 +2,9 @@
 
 const Plugin = require('release-it/lib/plugin/Plugin'),
     calver = require('calver'),
-    DEFAULT_FORMAT = 'YY.MM.MICRO';
+    DEFAULT_FORMAT = 'YY.MM.MICRO',
+    DEFAULT_INCREMENT = 'calendar',
+    FALLBACK_INCREMENT = 'micro';
 
 class CalverPlugin extends Plugin {
 
@@ -10,21 +12,30 @@ class CalverPlugin extends Plugin {
         return this.getContext().format || DEFAULT_FORMAT;
     }
 
-    getIncrementedVersion({latestVersion}) {
+    getInc() {
+        return this.getContext().increment || DEFAULT_INCREMENT;
+    }
+
+    getFallbackInc() {
+        return this.getContext().fallbackIncrement || FALLBACK_INCREMENT;
+    }
+
+    getIncrementedVersion(args) {
+        const {latestVersion} = args || {};
         calver.init(this.getFormat());
         try {
-          return calver.inc(this.getFormat(), latestVersion, 'calendar');
+          return calver.inc(this.getFormat(), latestVersion, this.getInc());
         } catch (e) {
-          return calver.inc(this.getFormat(), latestVersion, 'micro');
+          return calver.inc(this.getFormat(), latestVersion, this.getFallbackInc());
         }
     }
 
     getIncrementedVersionCI() {
-        return this.getIncrementedVersion(...arguments);
+      return this.getIncrementedVersion(...arguments);
     }
 
     getIncrement() {
-        return this.getIncrementedVersion(...arguments);
+      return this.getIncrementedVersion(...arguments);
     }
 }
 
